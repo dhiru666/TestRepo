8be94baf48eddaf657a28f6184c5c69039571ecb
dhiru666 <dhiraj.sahu@ajatus.co.in>
2021-08-10 11:36:17 +0530

    Bank file modified

--- Bank.java
+++ Bank.java
@@ -1,1 +1,19 @@
 
+public class Bank{
+
+
+public static void main(String[] args){
+
+int[] a=new int[]{10,20,30};
+
+
+for (int i=0;i<a.length ; i++) {
+	System.out.println(a[i]);
+}
+
+
+
+}
+
+
+}
