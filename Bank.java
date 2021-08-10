9be5dfc9ab214c7ecd2813fa41de5f963afede02
dhiru666 <dhiraj.sahu@ajatus.co.in>
2021-08-10 12:03:13 +0530

    bank updated with SB

--- Bank.java
+++ Bank.java
@@ -2,6 +2,14 @@
 public class Bank{
 
 
+static{
+
+System.out.println("in SB ");
+
+}
+
+
+
 public static void main(String[] args){
 System.out.println(""main start ");
 int[] a=new int[]{10,20,30};
@@ -12,6 +20,9 @@ for (int i=0;i<a.length ; i++) {
 }
 
 System.out.println(""main end ");
+
+
+
 
 }
 
